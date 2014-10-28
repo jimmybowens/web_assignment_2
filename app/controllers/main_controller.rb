0f2f7b3719ee5197c08baf6411738befcd8c8a7e
skaza <siddharthkaza@hotmail.com>
2014-10-28 17:51:18 -0400

    changed main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,22 +2,22 @@ class MainController < ApplicationController
   def index
     render :text=>"Hello! This is the ORM Lab. You must have the environment running."
   end
-
-  def add_records
-    #************************************************************
-    #        DO NOT RUN THIS METHOD FROM THE BROWSER
-    #               FOLLOW STEPS IN THE LAB
-    #************************************************************
-
-
+#************************************************************
+#        DO NOT RUN THESE METHODS FROM THE BROWSER
+#               FOLLOW STEPS IN THE LAB
+#************************************************************
+  def create_customer
     Customer.create(:name => 'John Doe', :email => "john@doe.com" , :balance => 78.29)
     Customer.create(:name => 'Bert Public' , :email => "b@public.net" , :balance => 12.45)
 
+  def create_manager
     wilma = Manager.create(:name => 'Wilma Flint' , :email => "wilma@here.com" , :dept => 23)
+  end
+
+  def create_employee
     barney = Employee.new(:name => 'Barney Rubble' , :email => "barney@here.com" , :dept => 23)
 
     barney.boss=wilma
     barney.save!
-
   end
 end
